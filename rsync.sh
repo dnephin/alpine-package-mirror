d06e79736c6430e6342c808c1612d505538efb01
Daniel Nephin <dnephin@gmail.com>
2015-07-30 20:10:06 -0700

    Fix rsync script.

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -e
 
 # make sure we never run 2 rsync at the same time
 lockfile="/tmp/alpine-mirror.lock"
@@ -12,9 +12,9 @@ src=rsync://rsync.alpinelinux.org/alpine/
 dest=/var/www/localhost/htdocs/alpine/
 
 /usr/bin/rsync -prua \
-        --exclude-from /etc/rsync/exclude.txt \
-        --delete \
-        --timeout=120 \
-        --delay-updates \
-        --delete-after \
-        "$src" "$dest"
+    --exclude-from /etc/rsync/exclude.txt \
+    --delete \
+    --timeout=120 \
+    --delay-updates \
+    --delete-after \
+    "$src" "$dest"
